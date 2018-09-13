a134831c46eae0376a7883ee7bb464454992cf59
KRogLA <i@krogla.me>
2018-09-13 17:25:50 +0500

    added total sums, design improvements

--- src/components/MyDividends.vue
+++ src/components/MyDividends.vue
@@ -17,35 +17,53 @@
 
     <b-card-group deck>
       <b-card border-variant="success" text-variant="white" sub-title="TOKENS" class="mb-2">
-        <h6 v-if="contractState"><b-badge>{{tkn_contract.address}}</b-badge></h6>
+        <p><small v-if="contractState" class="text-muted">Contract: <b-link :href="'https://etherscan.io/address/'+tkn_contract.address">{{tkn_contract.address}}</b-link></small></p>
         <!--<p class="card-text">You owned <strong>{{getBalance}}</strong> of <strong>{{getTotalSupply}}</strong> dividend tokens</p>-->
-        <h5>You owned <strong>{{getBalance}}</strong> of <strong>{{getTotalSupply}}</strong> UnicornGO Dividend Tokens (UDT)</h5>
+        <h5 class="fix">You owned <b-badge variant="success" pill>{{getBalance}}</b-badge> of <b-badge pill class="fix">{{getTotalSupply}}</b-badge> UnicornGO Dividend Tokens (UDT)</h5>
       </b-card>
       <b-card border-variant="primary" text-variant="white" sub-title="WITHDRAWAL" class="mb-2">
-        <h6 v-if="contractState"><b-badge>{{mgr_contract.address}}</b-badge></h6>
+        <p><small v-if="contractState" class="text-muted">Contract: <b-link :href="'https://etherscan.io/address/'+mgr_contract.address">{{mgr_contract.address}}</b-link></small></p>
+
+        <!--<h6 v-if="contractState"><small>Contract:</small> <b-badge>{{mgr_contract.address}}</b-badge></h6>-->
         <!--<p class="card-text">You can withdraw <strong>{{getPendingWithdrawal}}&Xi;</strong></p>-->
-        <h5>You can withdraw <strong>{{getPendingWithdrawal}}&Xi;</strong></h5>
+        <h5 class="fix">You can withdraw <b-badge variant="primary" pill>{{getPendingWithdrawal}}<svg class="symbol"><use xlink:href="#ethereum"></use></svg></b-badge></h5>
         <b-button variant="outline-primary" :disabled="withdrawState" @click="withdraw">Withdraw</b-button>
       </b-card>
     </b-card-group>
     <b-card-group deck>
-      <b-card border-variant="warning" sub-title="YOUR RECENT WITHDRAWALS" class="mb-2">
-        <b-table striped small fixed :items="listWithdrawals" v-if="listWithdrawals.length"></b-table>
+      <b-card border-variant="warning" text-variant="white" sub-title="YOUR RECENT WITHDRAWALS" class="mb-2">
+        <template v-if="isWithdrawals">
+          <h5 class="fix">Total:&nbsp;<b-badge variant="warning" pill>{{getWithdrawalsSum}}<svg class="symbol"><use xlink:href="#ethereum"></use></svg></b-badge></h5>
+          <b-table striped small fixed :items="getWithdrawals"></b-table>
+        </template>
         <p class="card-text" v-else>list is empty</p>
         <EllipsisSpinner v-if="loadingWithdrawals"/>
       </b-card>
-      <b-card border-variant="danger" sub-title="YOUR RECENT EARNINGS" class="mb-2">
-        <b-table striped small fixed :items="listEarnings" v-if="listEarnings.length"></b-table>
+      <b-card border-variant="danger" text-variant="white" sub-title="YOUR RECENT EARNINGS" class="mb-2">
+        <!--<b-table striped small fixed :items="listEarnings" v-if="listEarnings.length"></b-table>-->
+        <template v-if="isEarnings">
+          <h5 class="fix">Total:&nbsp;<b-badge variant="danger" pill>{{getEarningsSum}}<svg class="symbol"><use xlink:href="#ethereum"></use></svg></b-badge></h5>
+          <b-table striped small fixed :items="getEarnings"></b-table>
+        </template>
+
         <p class="card-text" v-else>list is empty</p>
         <EllipsisSpinner v-if="loadingEarnings"/>
       </b-card>
-      <b-card border-variant="info" sub-title="TOTAL RECENT DIVIDENDS INCOME" class="mb-2">
-        <b-table striped small fixed :items="listDividends" v-if="listDividends.length"></b-table>
+      <b-card border-variant="info" text-variant="white" sub-title="TOTAL RECENT DIVIDENDS INCOME" class="mb-2">
+        <!--<h5>Total sum: {{getDividendsSum}}</h5>-->
+        <!--<b-table striped small fixed :items="getDividends"></b-table>-->
+        <template v-if="isDividends">
+          <h5 class="fix">Total:&nbsp;<b-badge variant="info" pill>{{getDividendsSum}}<svg class="symbol"><use xlink:href="#ethereum"></use></svg></b-badge></h5>
+          <b-table striped small fixed :items="getDividends"></b-table>
+        </template>
         <p class="card-text" v-else>list is empty</p>
         <EllipsisSpinner v-if="loadingDividends"/>
       </b-card>
     </b-card-group>
-
+    <div style="display: none">
+      <svg id="ethereum" viewBox="0 0 2.646 3.704" width="100%" height="100%"><title>ethereum</title><path d="M1.323 0l-.025.084v2.45l.025.024 1.137-.672z"></path><path d="M1.323 2.558V0L.186 1.886zm0 .216l-.014.017v.872l.014.041L2.46 2.102z"></path><path d="M1.323 2.774L.186 2.102l1.137 1.602z"></path></svg>
+      <!--<svg id="candy" viewBox="0 0 2.381 3.704" width="100%" height="100%"><title>candy</title><path d="M1.794.48s.014-.657-.347-.12C1.357.244.992-.05.897.007c-.096.058.18.57.249.824-.377.055-.717.365-.834.803-.118.438.022.876.32 1.112-.185.185-.688.455-.627.589.06.133.518.025.653-.03.045.645.36.07.36.07s.54.528.506.247c-.03-.262-.178-.535-.272-.709.378-.052.72-.363.838-.803.118-.44-.023-.88-.325-1.114.17-.104.433-.267.591-.478.17-.226-.562-.038-.562-.038zm-.572 2.664c.017.14-.307-.202-.307-.202-.067.027-.3.13-.343.078-.037-.046.087-.116.18-.196a.81.81 0 0 0 .38.097.482.482 0 0 1 .09.223zm.062-2.096a.015.015 0 0 1-.013.011c-.318.037-.595.297-.689.647-.094.35.016.714.274.905a.015.015 0 0 1 .003.02.013.013 0 0 1-.017.006.708.708 0 0 1-.364-.385.913.913 0 0 1-.033-.582.913.913 0 0 1 .32-.489.709.709 0 0 1 .51-.15c.007.001.01.01.009.017zM1.855.78a.482.482 0 0 1-.19.148.81.81 0 0 0-.376-.106C1.248.708 1.176.585 1.23.563c.064-.024.214.182.258.24 0 0 .452-.135.367-.022z" stroke-width="17.65"></path></svg>-->
+    </div>
   </b-container>
 </template>
 
@@ -86,6 +104,52 @@
       isLoading() {
         return this.loadingWithdrawals || this.loadingDividends || this.loadingEarnings
       },
+
+      getDividendsSum() {
+        return ethers.utils.formatEther(this.listDividends.reduce((sum, i) => sum.add(i.incomePayment), ethers.utils.bigNumberify(0)))
+      },
+      getDividends() {
+        return this.listDividends.map(i => {
+          return {
+            blockNumber: i.blockNumber,
+            amount: ethers.utils.formatEther(i.incomePayment)
+          }
+        })
+      },
+      isDividends() {
+        return this.listDividends.length > 0
+      },
+
+      getWithdrawalsSum() {
+        return ethers.utils.formatEther(this.listWithdrawals.reduce((sum, i) => sum.add(i.amount), ethers.utils.bigNumberify(0)))
+      },
+      getWithdrawals() {
+        return this.listWithdrawals.map(i => {
+          return {
+            blockNumber: i.blockNumber,
+            amount: ethers.utils.formatEther(i.amount)
+          }
+        })
+      },
+      isWithdrawals() {
+        return this.listWithdrawals.length > 0
+      },
+      getEarningsSum() {
+        return ethers.utils.formatEther(this.listEarnings.reduce((sum, i) => sum.add(i.amount), ethers.utils.bigNumberify(0)))
+      },
+      getEarnings() {
+        return this.listEarnings.map(i => {
+          return {
+            blockNumber: i.blockNumber,
+            amount: ethers.utils.formatEther(i.amount)
+          }
+        })
+      },
+      isEarnings() {
+        return this.listEarnings.length > 0
+      },
+
+
 
       getBalance() {
         return ethers.utils.formatUnits(this.balance, 3)
@@ -189,7 +253,8 @@
               let r = this.mgr_contract.interface.events.WithdrawalAvailable.parse(l.topics, l.data)
               return {
                 blockNumber: l.blockNumber,
-                amount: ethers.utils.formatEther(r.amount)
+                // amount: ethers.utils.formatEther(r.amount)
+                amount: r.amount
               }
             })
           this.loadingEarnings = false
@@ -225,7 +290,8 @@
               let r = this.mgr_contract.interface.events.WithdrawalPayed.parse(l.topics, l.data)
               return {
                 blockNumber: l.blockNumber,
-                amount: ethers.utils.formatEther(r.amount)
+                // amount: ethers.utils.formatEther(r.amount)
+                amount: r.amount
               }
             })
           this.loadingWithdrawals = false
@@ -253,7 +319,8 @@
               let r = this.mgr_contract.interface.events.DividendPayment.parse(l.topics, l.data)
               return {
                 blockNumber: l.blockNumber,
-                incomePayment: ethers.utils.formatEther(r.paymentPerShare.mul(this.totalSupply))
+                // incomePayment: ethers.utils.formatEther(r.paymentPerShare.mul(this.totalSupply))
+                incomePayment: r.paymentPerShare.mul(this.totalSupply)
               }
             })
           this.loadingDividends = false
@@ -319,6 +386,23 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only lang="sass"-->
-<!--<style scoped>-->
+<style lang="scss" scoped>
 
-<!--</style>-->
+  h5.fix, .h5.fix {
+    font-size: 1.2rem;
+    .badge {
+      font-size: 90%;
+    }
+  }
+  .symbol {
+    display: inline-block;
+    position: relative;
+    fill: currentColor;
+    width: 1rem;
+    height: 1rem;
+    margin-top: -0.2rem;
+    /*margin-right: 0.3rem;*/
+    /*margin-left: 0.3rem;*/
+    vertical-align: middle;
+  }
+</style>
